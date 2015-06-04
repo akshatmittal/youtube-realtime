9c55e8166ee6a631870375138b51fe87e302cf9a
Akshat Mittal <itsreallyakshat@gmail.com>
2015-06-04 12:55:14 +0530

    Analytics + LazyLoad

--- script.js
+++ script.js
@@ -117,4 +117,13 @@ window.onload = function() {
 	setInterval(update.live, 1*1000);
 	setInterval(update.yt, 60*1000);
 	document.querySelector("#username").onclick = newUsername;
+	// Social!
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=1473140929606808";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 }
