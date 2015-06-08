af0dc2856695ad2e07c82247dac06397b116c8b6
Akshat Mittal <itsreallyakshat@gmail.com>
2015-06-08 15:02:43 +0530

    Better load time - move Analytics

--- script.js
+++ script.js
@@ -114,9 +114,12 @@ window.onload = function() {
 		}, 3000);
 	}
 	update.all();
-	setInterval(update.live, 1*1000);
-	setInterval(update.yt, 60*1000);
+	if(location.hostname != "localhost") {
+		setInterval(update.live, 1*1000);
+		setInterval(update.yt, 60*1000);
+	}
 	document.querySelector("#username").onclick = newUsername;
+	
 	// Social!
 	(function(d, s, id) {
 	  var js, fjs = d.getElementsByTagName(s)[0];
@@ -126,4 +129,10 @@ window.onload = function() {
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-50190232-6', 'auto');
+	ga('send', 'pageview');
 }
