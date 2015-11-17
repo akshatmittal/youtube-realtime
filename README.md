be4fba9e576e8a5150d7a5e9983f84529a520d9f
Akshat Mittal <itsreallyakshat@gmail.com>
2015-11-17 17:40:01 +0530

    YouTube Realtime v2.0

--- README.md
+++ README.md
@@ -2,32 +2,50 @@
 
 This is a simple experiment which shows the current live subscriber count on YouTube for a user. The count displayed on YouTube is often incorrect because it doesn't update in realtime. The count used on this experiment is taken directly from the API and hence is guaranteed to be accurate.
 
-The design follows Material Guidelines by Google, although could be mildly displaced since no libraries were used for the same. The page is only 20KB in size, including all resources used. This page is super light and the follow up network requests are as minimal as possible.
+The design follows Material Guidelines by Google, although could be mildly displaced since no libraries were used for the same. This page is super light and the follow up network requests are as minimal as possible.
 
-You can directly bookmark the relevant page to directly jump to a specific user, and while sharing you can also use youtuberealtime.ga/<username or channel ID>
+You can directly bookmark the relevant page to directly jump to a specific user. Sharing features are built right into the web app.
 
-![YouTube Realtime Screenshot](http://i.imgur.com/B317Q5e.png)
+![YouTube Realtime Screenshot](res/Example1.png)
 
 Feel free to submit issues/pull request if you'd like to contribute. No javascript or theming libraries were used (except Odometer for smooth digit animations) and I prefer not to, please keep this in mind while submitting a pull request.
 
 I've been seeing a lot of copies of this project on the internet, please consider adding an attribution link if you are planning to deploy this project on your website. If you are a YouTuber, consider using a direct link for your channel instead of creating a new clone website. Thanks!
 
+# Screenshots
+![YouTube Realtime Screenshot](res/Example2.png)
+*YouTube Realtime showing a self discovery channel*
+
+![YouTube Realtime Screenshot](res/ExampleDark.png)
+*YouTube Realtime with Dark Theme enabled*
+
+![YouTube Realtime Screenshot](res/ExampleMilestone.png)
+*YouTube Realtime with Milestones enabled*
+
+# Features
+
+* Realtime Subscriber Count
+* Aesthetically pleasing design and colors
+* Easy channel selection
+* Shows data on YouTube directly on page
+* Shows total video views of the user
+* Easy Sharing built in
+* Dark Theme
+* Milestones
+* Simple custom URls
+
 # Try it yourself!
 
-* [Sky Does Minecraft](https://akshatmittal.github.io/youtube-realtime/#!/SkyDoesMinecraft "Sky Does Minecraft's YouTube Subscriber Count")
-* [PewDiePie](https://akshatmittal.github.io/youtube-realtime/#!/PewDiePie "PewDiePie's YouTube Subscriber Count")
-* [Smosh](https://akshatmittal.github.io/youtube-realtime/#!/Smosh "Smosh's YouTube Subscriber Count")
-* [Marques Brownlee](https://akshatmittal.github.io/youtube-realtime/#!/MarquesBrownlee "Marques Brownlee's YouTube Subscriber Count")
-
-# Todo
-
-1. Mobile View (Kinda implemented)
-2. Embeds
-3. ~~Support for Channels~~
-4. *Anything else?*
+* [Sky Does Minecraft](https://akshatmittal.com/youtube-realtime/#!/SkyDoesMinecraft "Sky Does Minecraft's Realtime Subscriber Count on YouTube")
+* [PewDiePie](https://akshatmittal.com/youtube-realtime/#!/PewDiePie "PewDiePie's Realtime Subscriber Count on YouTube")
+* [Smosh](https://akshatmittal.com/youtube-realtime/#!/Smosh "Smosh's Realtime Subscriber Count on YouTube")
+* [Marques Brownlee](https://akshatmittal.com/youtube-realtime/#!/MarquesBrownlee "Marques Brownlee's Realtime Subscriber Count on YouTube")
+* [#Music](https://akshatmittal.com/youtube-realtime/#!/UC-9-kyTW8ZkZNDHQJ6FgpwQ "#Music's Realtime Subscriber Count on YouTube")
 
 # License
 
-YouTube Realtime Copyright (C) 2015 [Akshat Mittal](http://akshatmittal.com/)
+YouTube Realtime Copyright (C) 2015 [Akshat Mittal](https://akshatmittal.com/)
+
+You are allowed to use, modify and redistribute the project as long as original credits are kept and changes are documented. You are not allowed to redistribute the project without attribution and credits or prior permission.
 
 Please see LICENSE.md file in the same directory.
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+var baseURL = "https://akshatmittal.com/youtube-realtime/";
+if(window.top !== window.self) window.top.location.replace(window.self.location.href);
+if(location.hostname != "akshatmittal.com" && location.hostname != "localhost") location.replace(baseURL + location.hash);
+if(location.protocol != "https:" && location.hostname != "localhost") location.protocol = "https:";
+
 var coolGuys = ['PewDiePie','Smosh','MarquesBrownlee','YouTube','TaylorSwiftVEVO','EminemVEVO','BuzzFeedVideo','Smosh', 'Machinima', 'SkyDoesMinecraft'];
 var username = coolGuys[Math.floor(Math.random()*coolGuys.length)];
+var rawInput = username;
+var keyIndex = 0;
 
 var getText = function(url, callback) {
 	var request = new XMLHttpRequest();
@@ -11,6 +18,22 @@ var getText = function(url, callback) {
 	request.open('GET', url);
 	request.send();
 }
+var toggleDark = function() {
+	var html = document.body.parentElement;
+	if(html.className == "dark") {
+		html.className = "";
+	} else {
+		html.className = "dark";
+	}
+}
+var toggleMilestones = function() {
+	var el = document.getElementById('milestoneBox');
+	if(el.style.maxHeight == "5em") {
+		el.style.maxHeight = "0";
+	} else {
+		el.style.maxHeight = "5em";
+	}
+}
 var changeText = function(elem, changeVal) {
     if ('textContent' in elem) {
         elem.textContent = changeVal;
@@ -18,32 +41,49 @@ var changeText = function(elem, changeVal) {
         elem.innerText = changeVal;
     }
 }
+var orientationCheck = function() {
+	if(window.innerWidth < window.innerHeight) { // Device is in Potrait mode
+		document.getElementById('deviceOrientation').style.visibility = "visible";
+	} else {
+		document.getElementById('deviceOrientation').style.visibility = "hidden";
+	}
+}
 
 var update = {};
-update.isNameSet = 0;
-update.name = function() {
-	if(this.isNameSet)
-		return;
-	var channelType = (username.length>=24 && username.substr(0, 2).toUpperCase() == "UC")?"channel":"user";
-	var url = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2F"+channelType+"%2F"+username+"%2Fabout%22%20%0AAND%20xpath%3D'%2F%2F*%5B%40class%3D%22qualified-channel-title-text%22%5D%2F%2Fa'&format=json"
-	getText(url, function(e){
+update.name = function(name) {
+	document.title = name + "'s Realtime Subscriber Count on YouTube";
+	changeText(document.querySelector("#username"), name);
+}
+update.queryName = function() {
+	getText("https://www.googleapis.com/youtube/v3/search?part=snippet&q="+encodeURIComponent(rawInput)+"&type=channel&maxResults=1&key=" + update.getKey(), function(e){
 		e = JSON.parse(e);
-		var name = e.query.results.a.content;
-		var u = document.querySelector("#username");
-		document.title = name + "'s YouTube Subscriber Count";
-		changeText(u, name);
-		u.setAttribute("title", username);
-		update.isNameSet = 1;
-	});
+		if(e.pageInfo.totalResults < 1) {
+			newUsername("#Music", "Could not find any channel with that name. ");
+			return;
+		}
+		var n = e.items[0].snippet.title;
+		update.name(n);
+	})
+}
+update.currentMilestone = function(subs) {
+	var targets = [10, 25, 50, 1E2, 5E2, 1E3, 2.5E3, 5E3, 1E4, 2.5E4, 5E4, 1E5, 2.5E5, 1E6, 2E6, 3E6, 5E6, 10E6, 15E6, 20E6, 25E6, 30E6, 40E6, 50E6, 70E6, 1E8, 1.5E8, 2E8, 3E8];
+	var target = 0;
+	for(var i = 0;i<targets.length;i++) {
+		if(subs <= targets[i]) {
+			target = targets[i];
+			break;
+		}
+	}
+	return Array(target, target - subs);
 }
 update.isLive = 0;
 update.live = function() {
-	// Please don't use this API key in your projects.
 	var reqType = (username.length>=24 && username.substr(0, 2).toUpperCase() == "UC")?"id":"forUsername";
-	var url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&"+reqType+"="+username+"&fields=items/statistics/subscriberCount&key=AIzaSyC0QYUSKEwHaRVz4NKpT1SLbkVMT1o5cM8";
+	var url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&"+reqType+"="+username+"&fields=items/statistics/subscriberCount&key=" + update.getKey();
 	getText(url, function(e) {
 		e = JSON.parse(e);
 		var sub_count = e.items[0].statistics.subscriberCount;
+		var m = update.currentMilestone(sub_count);
 		if(!update.isLive) {
 			new Odometer({
 				el: document.querySelector(".count_live"),
@@ -51,12 +91,26 @@ update.live = function() {
 				format: '(,ddd)',
 				theme: 'minimal'
 			});
+			//changeText(document.querySelector(".milestone_target"), m[0].toLocaleString("en-US"));
+			new Odometer({
+				el: document.querySelector(".milestone_target"),
+				value: m[0],
+				format: '(,ddd)',
+				theme: 'minimal'
+			});
+			new Odometer({
+				el: document.querySelector(".milestone_away"),
+				value: m[1],
+				format: '(,ddd)',
+				theme: 'minimal'
+			});
 			update.isLive = 1;
 		} else {
 			changeText(document.querySelector(".count_live"), sub_count);
+			changeText(document.querySelector(".milestone_target"), m[0].toLocaleString("en-US"));
+			changeText(document.querySelector(".milestone_away"), m[1]);
 		}
-	})
-
+	});
 }
 update.isYt = 0;
 update.yt = function() {
@@ -70,7 +124,7 @@ update.yt = function() {
 			var count_view = bT[1];
 		} else {
 			var count_subs = bT;
-			var count_view = 0;
+			var count_view = "0";
 		}
 		if(count_subs.indexOf(decodeURIComponent("%C2%A0")) > -1) {
 			count_subs = count_subs.split(decodeURIComponent("%C2%A0")).join("");
@@ -102,51 +156,90 @@ update.yt = function() {
 		}
 	});
 }
+update.parseInput = function(a) {
+	rawInput = a;
+	getText("https://www.googleapis.com/youtube/v3/search?part=snippet&q="+encodeURIComponent(a)+"&type=channel&maxResults=1&key=" + update.getKey(), function(e){
+		e = JSON.parse(e);
+		if(e.pageInfo.totalResults < 1) {
+			newUsername("#Music", "Could not find any channel with that name. ");
+			return;
+		}
+		var u = (e.items[0].snippet.channelTitle != undefined && e.items[0].snippet.channelTitle.trim() != "")?e.items[0].snippet.channelTitle:e.items[0].snippet.channelId;
+		var n = e.items[0].snippet.title;
+		update.reset(u);
+		update.name(n);
+	})
+}
+update.share = function(a) {
+	var sharableLink = encodeURIComponent(document.getElementById('shareURL').value);
+	var facebook = "https://www.facebook.com/dialog/feed?app_id=1473140929606808&display=page&caption=Realtime%20Subscriber%20Count&link=" + sharableLink + "&redirect_uri=" + encodeURIComponent(baseURL + "close.html");
+	var twitter = "https://twitter.com/intent/tweet?original_referer=" + sharableLink + "&ref_src=twsrc%5Etfw&text="+encodeURIComponent(document.title)+"&tw_p=tweetbutton&via=itsreallyakshat&url=" + sharableLink;
+	switch (a) {
+		case 'twtr':
+			window.open(twitter);
+			break;
+		case 'fb':
+			window.open(facebook);
+			break;
+		default:console.log("That's not how it works.");
+	}
+}
+update.getKey = function() {
+	var APIkeys = ["AIzaSyC0QYUSKEwHaRVz4NKpT1SLbkVMT1o5cM8", "AIzaSyBSyBp1KHjjXox6e9FBPoOCE1mbLvVUzUM"];
+	keyIndex = (keyIndex + 1)%(APIkeys.length);
+	return APIkeys[keyIndex];
+}
 update.all = function() {
-	update.name();
+	document.getElementById('shareURL').value = baseURL + "#!/" + username;
+	update.queryName();
 	update.live();
 	update.yt();
 }
 update.reset = function(a) {
-	username = a.trim();
 	if(!a) return;
-	update.isNameSet = 0;
+	if(a.trim() == username)
+		return;
+	username = a.trim();
+	history.pushState(null, null, "#!/" + username);
+	changeText(document.getElementById('username'), "..wait..");
 	update.all();
 	ga('send', 'pageview', {
-  	'page': location.pathname + location.search + location.hash
+  	'page': location.pathname + location.search + location.hash,
+		'title': document.title
 	});
 }
-function newUsername() {
-	var te = prompt("Enter new Username/Channel ID:", username);
+function newUsername(a, b) {
+	var te = prompt(((typeof(b) == "string")?b:"") + "Enter new user:", (typeof(a) == "string")?a:username);
 	if(te.trim() == username || te.trim() == "")
 		return;
 	if(te)
-		update.reset(te.trim());
-	changeText(this, "..wait..");
+		update.parseInput(te.trim());
+	changeText(document.getElementById('username'), "..wait..");
 	history.pushState(null, null, "#!/" + username);
 }
 window.onpopstate = function() {
 	var te = location.hash.split("!/")[1];
-	if(te)
+	if(te) {
 		username = te.trim();
-	changeText(document.querySelector('#username'), "..wait..");
-	update.reset(username);
+		rawInput = username;
+		changeText(document.querySelector('#username'), "..wait..");
+		update.queryName();
+	}
 }
 window.onload = function() {
 	var te = location.hash.split("!/")[1];
-	if(te)
+	if(te){
 		username = te.trim();
-	else {
-		location.replace("#!/" + username);
-		document.querySelector(".notice").style.visibility = "visible";
-		setTimeout(function(){
-			document.querySelector(".notice").className += " notice-hidden";
-		}, 3000);
+		rawInput = username;
+	}	else {
+		history.pushState(null, null, "#!/" + username);
 	}
 	update.all();
 	setInterval(update.live, 1*1000);
 	setInterval(update.yt, 60*1000);
 	document.querySelector("#username").onclick = newUsername;
+	window.onresize = orientationCheck;
+	orientationCheck();
 
 	// Social!
 	(function(d, s, id) {
@@ -164,6 +257,7 @@ window.onload = function() {
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 	ga('create', 'UA-50190232-6', 'auto');
 	ga('send', 'pageview', {
-  	'page': location.pathname + location.search + location.hash
+  	'page': location.pathname + location.search + location.hash,
+		'title': document.title
 	});
 }
