a123ebc97cde6ff5ab26ccca04725b740e292b62
Akshat Mittal <Akshat Mittal>
2016-06-15 15:24:58 +0530

    Embeds

--- assets/js/embed.js
+++
@@ -1,1 +0,0 @@
-var isEmbed = 1;
--- assets/js/script.js
+++ assets/js/script.js
@@ -357,6 +357,11 @@ window.onload = function() {
 		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://apis.google.com/js/platform.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","google-gjs");
 	}
+	if(isEmbed) {
+		document.body.onclick = function() {
+			window.open(baseURL + "#!/" + username);
+		}
+	}
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
