2d0653bf5b8a3ae956cd8527748ac6427d329f43
jfrenl <53793886+jfrenl@users.noreply.github.com>
2022-08-01 23:33:04 +0200

    Update query.vs.js

--- assets/js/query.vs.js
+++ assets/js/query.vs.js
@@ -1,7 +1,7 @@
 YT.query = {
     begin: function () {
-        $.getJSON("https://counts.live/api/youtube-subscriber-count/" + encodeURIComponent(YT.live.vs1) + "/data", function (f) {
-            $.getJSON("https://counts.live/api/youtube-subscriber-count/" + encodeURIComponent(YT.live.vs2) + "/data", function (g) {
+        $.getJSON("https://api.subscribercounter.nl/api/youtube-subscriber-count/" + encodeURIComponent(YT.live.vs1) + "/data", function (f) {
+            $.getJSON("https://api.subscribercounter.nl/api/youtube-subscriber-count/" + encodeURIComponent(YT.live.vs2) + "/data", function (g) {
                 YT.updateManager.updateCover(f.data.backdrop, g.data.backdrop);
                 YT.updateManager.updateName(f.data.name, g.data.name);
                 YT.updateManager.updateProfile(f.data.picture, g.data.picture);
