c3dc8c6dd00572c8484fd0379350226d6e7d2ca5
Electric <electricraichugv@gmail.com>
2025-06-19 19:33:13 -0400

    Update query.core.js

--- assets/js/query.core.js
+++ assets/js/query.core.js
@@ -1,41 +1,42 @@
 YT.query = {
-  newSearch(raw) {
-    const term = raw.trim();
-    if (!term) return;
+  newSearch: function (e) {
+    if (e.trim() == YT.live.channelID || e.trim() == "") {
+      return;
+    }
     YT.live.stop();
-
-    if (/^\d+$/.test(term)) {
-      $.getJSON(`/api/groups/${term}`, info => {
-        if (!info || !info.name) {
-          alert("No group found with ID " + term);
-          return location.href = baseURL;
+    if (e.trim().substr(0, 2).toUpperCase() == "UC" && e.trim().length >= 24) {
+      console.log(e);
+      $.getJSON("https://mixerno.space/api/roblox-group-counter/user/" + encodeURIComponent(e), function (f) {
+        if (!e) {
+          alert("No results found!");
+          location.href = baseURL;
+          return;
         }
-
-        YT.updateManager.updateGroupID(term);
-        YT.updateManager.updateName(info.name);
-        YT.updateManager.updateMembers(info.memberCount);
-        YT.updateManager.updateOwner(info.owner.username);
-
-        YT.urls.pushState(term);
+        YT.updateManager.updateChannelID(encodeURIComponent(e));
+        YT.updateManager.updateCover(f.user[2].count);
+        YT.updateManager.updateName(f.user[0].count);
+        YT.updateManager.updateProfile(f.user[1].count);
+        YT.urls.pushState(encodeURIComponent(e));
         YT.live.start();
       });
-    }
-    else {
-      YT.groupSearch.getResults(term);
+    } else {
+      $.getJSON("https://mixerno.space/api/youtube-channel-counter/search/" + encodeURIComponent(e), function (e) {
+        if (!e) {
+          alert("No results found!");
+          location.href = baseURL;
+          return;
+        }
+        YT.query.newSearch(e.list[0][2]);
+      });
     }
   },
-
-  search(e) {
+  search: function (e) {
     e.preventDefault();
-    const val = $("#yt_searchvalue").val();
-    this.newSearch(val);
+    YT.query.newSearch($("#yt_searchvalue").val());
     $("#yt_searchvalue").val("");
   },
-
-  bind() {
-    $("#yt_search").on("submit", e => this.search(e));
-    $("#yt_searchbutton").on("click", e => this.search(e));
-  }
+  bind: function () {
+    $("#yt_search").on("submit", this.search);
+    $("#yt_searchbutton").on("click", this.search);
+  },
 };
-
-YT.query.bind();
