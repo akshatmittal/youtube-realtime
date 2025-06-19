dcb07f16d5b14b292fe55764b99d9cc8ab3242aa
Electric <electricraichugv@gmail.com>
2025-06-19 19:23:01 -0400

    Update multisearch.core.js

--- assets/js/multisearch.core.js
+++ assets/js/multisearch.core.js
@@ -1,58 +1,71 @@
-YT.multisearch = {
-  getResults: function (e) {
+YT.groupSearch = {
+  getResults(query) {
+    const proxiedSearchUrl = 
+      "https://corsproxy.io/?" 
+      + encodeURIComponent(
+          "https://groups.roblox.com/v1/groups/search"
+          + "?keyword=" + query
+          + "&limit=10"
+        );
+
+    $.getJSON(proxiedSearchUrl, data => {
+      $("#results").empty();
+      (data.data || []).forEach(g => this.fetchDetails(g.id));
+    });
+  },
+
+  fetchDetails(groupId) {
     $.getJSON(
-      "https://api.subscribercounter.nl/api/youtube-subscriber-count/" + encodeURIComponent(e) + "/search",
-      function (e) {
-        $er = $("#results");
-        $er.html("");
-        e.data.forEach(function (f) {
-          if (f.id == YT.live.channelID) return;
-          $er.append(YT.multisearch.giveHtml(f.name, f.picture, f.id));
-        });
-      },
+      `/api/groups/${groupId}`, 
+      info => {
+        $.getJSON(
+          "https://thumbnails.roblox.com/v1/groups/icons"
+            + "?groupIds=" + groupId
+            + "&size=50x50&format=png&isCircular=true",
+          thumbData => {
+            const thumb = (thumbData.data[0]||{}).imageUrl;
+            $("#results").append(
+              YT.groupSearch.makeHtml(info.name, thumb, groupId)
+            );
+          }
+        );
+      }
     );
   },
-  giveHtml: function (name, image, id) {
-    $e = $("<div>", {
+
+  makeHtml(name, imageUrl, id) {
+    const $icon = $("<div>", {
       class: "round align-self-center",
-      style: "background: url('" + image + "');background-size:cover;",
+      style: `background:url('${imageUrl}') center/cover`
     });
-    $ee = $("<h3>", {
-      class: "m-b-0 font-light",
-    }).text(name);
-    $f = $("<div>", {
-      class: "m-l-10 align-self-center",
-    }).append($ee);
-    $g = $("<div>", {
-      class: "d-flex flex-row",
-    });
-    $g.append($e).append($f);
-    return $("<div>", {
-      class: "card-block card m-b-15",
-    })
-      .append($g)
-      .on("click", function () {
-        YT.multisearch.launchCompare(id);
+    const $label = $("<h3>", { class: "m-b-0 font-light" }).text(name);
+    const $info = $("<div>", { class: "m-l-10 align-self-center" })
+      .append($label);
+    return $("<div>", { class: "card-block card m-b-15" })
+      .append($("<div>", { class: "d-flex flex-row" })
+        .append($icon).append($info)
+      )
+      .on("click", () => {
+        window.open(`/group/${id}`);
+        this.reset();
       });
   },
-  resetCompare: function () {
-    $(".super-search,.dark-bg").fadeOut("400", function () {
-      $("#results").html("");
+
+  reset() {
+    $(".super-search,.dark-bg").fadeOut(400, () => {
+      $("#results").empty();
       $("#yt_searchvalue_m").val("");
     });
   },
-  newSearch: function (e) {
-    e.preventDefault();
-    YT.multisearch.getResults($("#yt_searchvalue_m").val());
-  },
-  launchCompare: function (e) {
-    if (e == YT.live.channelID) return;
-    window.open("/youtube-realtime/compare/#!/" + YT.live.channelID + "$$" + e);
-    this.resetCompare();
-  },
-  bind: function () {
-    $("#yt_comrest").on("click", this.resetCompare);
-    $("#yt_search_m").on("submit", this.newSearch);
-    $("#yt_searchbutton_m").on("click", this.newSearch);
-  },
+
+  init() {
+    $("#yt_comrest").on("click", this.reset);
+    $("#yt_search_m, #yt_searchbutton_m")
+      .on("submit click", e => {
+        e.preventDefault();
+        this.getResults($("#yt_searchvalue_m").val());
+      });
+  }
 };
+
+YT.groupSearch.init();
